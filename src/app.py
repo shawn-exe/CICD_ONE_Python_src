70d9e628de3331b31d0c662b34c94119fb99e81e
shawn-exe <shawnxsalis@gmail.com>
2026-04-26 16:29:17 +0530

    github-actions: set short sha for image versions

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ def info():
     return jsonify({
         'time': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
         'hostname': socket.gethostname(),
-        'env': 'test-dev'
+        'env': 'dev-env'
     })
 
 @app.route('/api/v1/healthz')
